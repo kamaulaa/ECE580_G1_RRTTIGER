1b7db0e0bb53e982c5cb841305774b030f31fa6b
Han Lee <jhl7@princeton.edu>
2025-11-21 20:11:42 -0500

    point array & nearest neighbor search

--- ECE580_Final_Project.srcs/sources_1/new/random_generator.v
+++ ECE580_Final_Project.srcs/sources_1/new/random_generator.v
@@ -7,45 +7,36 @@
 module rrt_random_point 
 #(
     // adjustable parameters to set grid boundaries
-    // should match grid.v
-    parameter X_BITS = 6,
-    parameter Y_BITS = 6,
-
-    parameter LFSR_BITS = 16 // any default value would do as long as >= X_BITS + Y_BITS
+    parameter X_BITS = 10,
+    parameter Y_BITS = 10,
+    parameter ADDR_BITS = 20 
 )
 (
     input                    clk,
     input                    rst,
     input                    generate_req, // pulse to get a new random point
-
-    output reg [X_BITS-1:0]  rand_x,
-    output reg [Y_BITS-1:0]  rand_y,
-    output reg               rand_valid
+    output [X_BITS-1:0]  random_point_x,
+    output [Y_BITS-1:0]  random_point_y
 );
 
-    reg [LFSR_BITS-1:0] lfsr = {LFSR_BITS{1'b1}}; // non-zero seed 
+    localparam LFSR_BITS = ADDR_BITS + 1; // any default value would do as long as >= X_BITS + Y_BITS
 
+    reg [LFSR_BITS-1:0] lfsr = {LFSR_BITS{1'b1}}; // non-zero seed 
     wire feedback = lfsr[LFSR_BITS-1] ^ lfsr[2] ^ lfsr[1] ^ lfsr[0]; // example taps
+    // Next LFSR value: only advance when generate_req is 1
+    wire [LFSR_BITS-1:0] lfsr_next = (generate_req==1'b1) ? {lfsr[LFSR_BITS-2:0], feedback} : lfsr;
+
+    assign random_point_x = lfsr[X_BITS-1:0];
+    assign random_point_y = lfsr[ADDR_BITS-1:X_BITS];
 
     always @(posedge clk) begin
         if (rst) begin
-            lfsr       <= {LFSR_BITS{1'b1}}; // non-zero feed
-            rand_x     <= {X_BITS{1'b0}};
-            rand_y     <= {Y_BITS{1'b0}};
-            rand_valid <= 1'b0;
+            lfsr <= {LFSR_BITS{1'b1}}; // non-zero feed
         end 
         else begin
-            rand_valid <= 1'b0;
-            if (generate_req) begin
-                // advance LFSR
-                lfsr <= {lfsr[LFSR_BITS-2:0], feedback};
-
-                // map bits to coordinates
-                rand_x     <= lfsr[X_BITS-1:0];
-                rand_y     <= lfsr[X_BITS+Y_BITS-1:X_BITS];
-                rand_valid <= 1'b1;
-            end
+            lfsr <= lfsr_next;
         end
     end
+    
 
 endmodule
